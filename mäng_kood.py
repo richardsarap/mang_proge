74a564b93e257ad1c872a8976e9d59b32866b51f
richardsarap <rikusarap@gmail.com>
2023-04-04 19:18:55 +0300

    midagi on valmis

--- mäng_kood.py
+++ mäng_kood.py
@@ -1,4 +1,4 @@
-import pygame as pg
+"""import pygame as pg
 import random
 import time
 pg.init()
@@ -10,31 +10,80 @@ pg.display.set_caption("Menüü")
 font = pg.font.Font('arial.ttf', 40)
 #C:/Users/richard.sarap/mang_proge/arial.ttf
 #VÄRVID
-
 tekst_värv = (255, 255, 255)
 def kirjuta(text, font, tekst_värv, x, y):
     pilt = font.render(text, True, tekst_värv)
     screen.blit(pilt, (x, y))
 
-
 screen = pg.display.set_mode((ekraani_laius, ekraani_pikkus))
 
+taust = pg.Surface((ekraani_laius, ekraani_pikkus))
+
 screen.fill((25, 26, 50))
+
 gameplay = True
+
 while gameplay:
-    kirjuta("Start", font, tekst_värv, 200, 100)
-    kirjuta("Press Space to continue", font, tekst_värv, 50, 200)
+    kirjuta("Mängu algus!", font, tekst_värv, 135, 100)
+    kirjuta("Press Space to continue", font, tekst_värv, 50, 400)
     for event in pg.event.get():
         if event.type == pg.QUIT:
             gameplay = False
             break
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_SPACE:
-                kirjuta("Läks", font, tekst_värv, 200, 300)
-
-
-    #pg.draw.circle(screen, (255, 0, 0), (250, 250), 50, 50)
+                print("läks!")
 
 
 
-    pg.display.update()
+
+    pg.display.update()"""
+"""
+import pygame as pg
+import random
+import time
+pg.init()
+aeg = pg.time.Clock()
+
+screen = pg.display.set_mode((800,400))
+
+gameplay = True
+algus = True
+
+koordid = []
+skoor = 0
+
+ringi_raadius = random.randint(30,100)
+X_voimalik_positsioon = 800-ringi_raadius #X koordinaat kuhu ring saab tekkida arvestades tema raadiust
+Y_voimalik_positsioon = 400-ringi_raadius
+X_positsioon = random.randint(ringi_raadius, X_voimalik_positsioon) # suvaline X koordinaat kuhu ringi X tekib
+Y_positsioon = random.randint(ringi_raadius, Y_voimalik_positsioon)
+
+def ring():
+    pg.draw.circle(screen, (255, 0, 0), (X_positsioon, Y_positsioon), ringi_raadius, ringi_raadius)
+
+while gameplay:
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            gameplay = False
+            break
+        ring()
+        if event.type == pg.MOUSEBUTTONDOWN:
+            hiirePositsioon = pg.mouse.get_pos() # vaatab kas positsioon on sama mis ringidel oli
+            # (x,y)
+            X_positsioon = random.randint(ringi_raadius,X_voimalik_positsioon) # suvaline X koordinaat kuhu ringi X tekib
+            Y_positsioon = random.randint(ringi_raadius, Y_voimalik_positsioon)
+            väiksemX = X_positsioon + 2 * ringi_raadius
+
+            if hiirePositsioon[0] >= X_positsioon and hiirePositsioon[0] <= väiksemX:
+                väiksemY = Y_positsioon + 2 * ringi_raadius
+                if hiirePositsioon[1] >= Y_positsioon and hiirePositsioon[1] <= väiksemY:
+                        skoor += 1
+                        print(skoor)
+                        algus = True
+
+    screen.fill((255, 255, 255))
+    pg.draw.circle(screen, (255, 0, 0), (X_positsioon, Y_positsioon), ringi_raadius, ringi_raadius)
+    ring()
+    pg.display.update()"""
