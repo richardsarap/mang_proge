777b737c269c15df9a53622bd01c64778b62b3e3
richardsarap <rikusarap@gmail.com>
2023-04-30 17:21:01 +0300

    valmis

--- mäng_klassi_prooviks.py
+++ mäng_klassi_prooviks.py
@@ -1,7 +1,7 @@
 import pygame, sys, random
 
 pygame.init()
-laius, kõrgus = 600, 600
+laius, kõrgus = 800, 800
 screen = pygame.display.set_mode((laius, kõrgus))
 
 #font
@@ -22,17 +22,19 @@ peida_ruut = False
 jooned = False
 
 
-
-
 kõik_ruudud =[] #  salvestada koordinaadid ja teada, mitme ruudu koordinaadid seal on
 õiged = 0#  vaja, et peale ühte "levelit" saaks aru, et on vaja teha juurde üks ruut
 vajutused = 0#  loeb mitu korda on vajutatud ühes levelis ja vaatab, kas vajutatakse õiges järjeskorras
 i = 0# vaja põhimõtteliselt selleks, et aru saada, kas on veel vaja ruute kuvada või mitte
 
+screen.fill((71, 30, 255))
 algus_tekst = draw_text("Space, et alusatada", font, TEXT_COL, 60, 250)
-algus_tekst = True
 
-while True:
+algus_tekst = True
+elud = 3
+mäng = True
+while mäng == True:
+
 
     pygame.time.Clock().tick(1)
     for event in pygame.event.get():
@@ -44,6 +46,15 @@ while True:
                 x, y = event.pos
                 if kõik_ruudud[vajutused][0] < x < kõik_ruudud[vajutused][0] + laius//5 and kõik_ruudud[vajutused][1] < y < kõik_ruudud[vajutused][1] + kõrgus//5:#   kontrollib, kas õigesse kohta vajutati
                    vajutused += 1
+                else:
+                    elud -= 1
+                    print("elud: " + str(elud))
+                    if elud == 0:
+                        mäng = False
+                        print("kaotasid")
+                    elif elud == 1:
+                        print("Ära nüüd mööda vajuta!")
+
 
             elif vajutused+1 == len(kõik_ruudud):
                 x, y = event.pos
@@ -52,10 +63,21 @@ while True:
                     õiged += 1
                     tee_ruut = True
                     vajutused = 0
+                else:
+                    elud -= 1
+                    print("elud: " + str(elud))
+
+                    if elud == 0:
+                        mäng = False
+                        print("kaotasid")
+                    elif elud == 1:
+                        print("Ära nüüd mööda vajuta!")
+
+
 
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                screen.fill((0, 0, 0))
+                screen.fill((71, 30, 255))
                 algus_tekst = False
                 tee_ruut = True
                 for u in range(5):
@@ -65,8 +87,9 @@ while True:
                                      (laius, kõrgus // 5 * u))  # vertikaalsed jooned
 
 
+
     if peida_ruut:#   Peidab ruudu
-        pygame.draw.rect(screen, (0, 0, 0), ((kõik_ruudud[i][0] + 2, kõik_ruudud[i][1] + 2), (laius // 5 - 2, kõrgus // 5 - 2)))#   all tehtud punase ruudu peale joonistatakse must ruut ja tahetakse vajutust
+        pygame.draw.rect(screen, (71, 30, 255), ((kõik_ruudud[i][0] + 2, kõik_ruudud[i][1] + 2), (laius // 5 - 2, kõrgus // 5 - 2)))#   all tehtud punase ruudu peale joonistatakse must ruut ja tahetakse vajutust
         if i+1 < len(kõik_ruudud):
             i += 1
             tee_ruut = True
